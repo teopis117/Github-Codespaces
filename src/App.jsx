af5d0f6fc3e673b920e38104e942f3ada37fce49
José Eduardo Díaz Morales <72634757+teopis117@users.noreply.github.com>
2023-06-04 21:52:36 +0000

    Primer cambio en el sistema

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "José Eduardo Díaz Morales",
+  title: "Computer Systems Engineer",
+  email: "IN PROGRESS....",
+  gitHub: "https://github.com/teopis117",
+  instagram: "https://www.instagram.com/unchicoconcamara117/",
+  linkedIn: "https://www.linkedin.com/in/diazmoralespepe117/",
 };
 
 const primaryColor = "#4E567E";
